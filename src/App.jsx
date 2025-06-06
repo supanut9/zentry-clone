a926dc4c4c098c96e3315ecec654826628004cc4
supanut9 <supanut.tanso@gmail.com>
2025-06-06 23:51:40 +0700

    feat(config): update tailwind theme fonts and colors

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './index.css';
 const App = () => {
   return (
     <main>
-      <h1 className='text-5xl text-orange-500 font-bold'>
+      <h1 className='text-5xl text-center text-violet-300 font-bold'>
         Welcome to Awwwards
       </h1>
     </main>
