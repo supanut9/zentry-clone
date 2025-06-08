88b1d120052ef9170b8899a79b20f03cc471c3d8
supanut9 <supanut.tanso@gmail.com>
2025-06-08 13:27:07 +0700

    feat(navbar): add Navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './index.css';
 
 import About from './components/About';
 import Hero from './components/Hero';
+import Nabar from './components/Nabar';
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Nabar />
       <Hero />
       <About />
     </main>
